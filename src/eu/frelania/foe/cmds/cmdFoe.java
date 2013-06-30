365cdc0e2e1bf62d4b067636243d4ce7f5aa6dab
siOnzeeSlav <siOnzee@frelania.eu>
2013-06-30 20:24:58 +0200

    Přidána verze.

--- src/eu/frelania/foe/cmds/cmdFoe.java
+++ src/eu/frelania/foe/cmds/cmdFoe.java
@@ -1,15 +1,32 @@
 package eu.frelania.foe.cmds;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import eu.frelania.foe.main.FoE;
+
 public class cmdFoe implements CommandExecutor {
+	
+	public FoE	foe;
+	
+	public cmdFoe(FoE plugin) {
+		foe = plugin;
+	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("foe") && args.length < 1) {
+			sender.sendMessage("--- " + ChatColor.GREEN + "Fundamentals of Elements" + ChatColor.WHITE + " ---");
+			cmd(sender, "Verze pluginu:", foe.getDescription().getVersion());
+			cmd(sender, "Verze online:", getOnlineVersion());
 			cmd(sender, "/foe", "Prikaz ktery zobrazi napovedu pro plugin FoE.");
 		} else if (cmd.getName().equalsIgnoreCase("foe") && args.length > 0) {
 			sender.sendMessage("Prikaz foe nepodporuje vice argumentu!");
@@ -18,7 +35,24 @@ public class cmdFoe implements CommandExecutor {
 	}
 	
 	public void cmd(CommandSender sender, String cmd, String info) {
-		sender.sendMessage(ChatColor.GREEN + cmd + ChatColor.WHITE + "  --  " + ChatColor.GOLD + info + ".");
+		sender.sendMessage(ChatColor.GREEN + cmd + ChatColor.WHITE + "  --  " + ChatColor.GOLD + info);
+	}
+	
+	public String getOnlineVersion() {
+		try {
+			URL url = new URL("http://www.frelania.eu/MyImages/FoE.txt");
+			URLConnection connection = url.openConnection();
+			
+			if (connection.getContentLength() != -1) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(new URL("http://www.frelania.eu/MyImages/FoE.txt").openStream()));
+				String newVersion = br.readLine();
+				br.close();
+				return newVersion;
+			}
+		} catch (Exception e) {
+			foe.getErrorMananger().log(Level.SEVERE, e);
+		}
+		return "Neni pripojeni k internetu";
 	}
 	
 }
